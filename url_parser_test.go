96598846403e1740d1187b5fbafbd2e459d5bd38
amix <amix@amix.dk>
2015-12-22 15:46:41 -0300

    Added extra test for a special formted string

--- url_parser_test.go
+++ url_parser_test.go
@@ -18,3 +18,18 @@ func TestBasicURLs(t *testing.T) {
 		}
 	}
 }
+
+func TestBugURL(t *testing.T) {
+	got := ParseURLs("Testing this out Bug report http://f.cl.ly/items/000V0N1B31283s3O350q/Screen%20Shot%202015-12-22%20at%2014.49.28.png")
+	want := []string{"http://f.cl.ly/items/000V0N1B31283s3O350q/Screen%20Shot%202015-12-22%20at%2014.49.28.png"}
+
+	if len(got) != len(want) {
+		t.Errorf("Length not the same got: %d != want: %d", len(got), len(want))
+	} else {
+		for i := 0; i < len(want); i++ {
+			if got[i] != want[i] {
+				t.Errorf("%q != %s", got, want)
+			}
+		}
+	}
+}
