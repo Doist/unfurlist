69838fe79368ab02030d55365ea3de7ec4ea5da0
Artyom Pervukhin <artyom@evasive.ru>
2015-12-25 14:11:54 +0000

    Expose pprof data

--- cmd/unfurlist/unfurlist.go
+++ cmd/unfurlist/unfurlist.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 
 	"bitbucket.org/doist/unfurlist"
@@ -14,10 +15,12 @@ import (
 func main() {
 	var (
 		listen            = "127.0.0.1:8080"
+		pprofListen       = "127.0.0.1:6060"
 		cache             = ""
 		certfile, keyfile string
 	)
 	flag.StringVar(&listen, "listen", listen, "`address` to listen, set both -sslcert and -sslkey for HTTPS")
+	flag.StringVar(&pprofListen, "pprof", pprofListen, "`address` to serve pprof data at (usually localhost)")
 	flag.StringVar(&certfile, "sslcert", "", "path to certificate `file` (PEM)")
 	flag.StringVar(&keyfile, "sslkey", "", "path to certificate key `file` (PEM)")
 	flag.StringVar(&cache, "cache", cache, "`address` to memcached client (both host and ip)")
@@ -40,6 +43,9 @@ func main() {
 	}
 
 	handler := unfurlist.New(&config)
+	if pprofListen != "" {
+		go func(addr string) { log.Println(http.ListenAndServe(addr, nil)) }(pprofListen)
+	}
 
 	if certfile != "" && keyfile != "" {
 		log.Fatal(http.ListenAndServeTLS(listen, certfile, keyfile, handler))
