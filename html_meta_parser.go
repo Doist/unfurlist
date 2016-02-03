cccbe90bc1a20c7aef87a00c839bc74f7b758c64
Artyom Pervukhin <artyom@evasive.ru>
2016-02-03 15:41:04 +0300

    Improve performance of title extraction
    Do less allocations by using low-level html.Tokenizer API.
    
    name           old time/op    new time/op    delta
    TitleParser-4    42.8µs ± 1%    34.2µs ± 1%  -20.05%  (p=0.008 n=5+5)
    
    name           old alloc/op   new alloc/op   delta
    TitleParser-4    57.9kB ± 0%    55.2kB ± 0%   -4.69%  (p=0.008 n=5+5)
    
    name           old allocs/op  new allocs/op  delta
    TitleParser-4      91.0 ± 0%      61.0 ± 0%  -32.97%  (p=0.008 n=5+5)

--- html_meta_parser.go
+++ html_meta_parser.go
@@ -6,10 +6,12 @@ package unfurlist
 import (
 	"bytes"
 	"errors"
+	"io"
 	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 	"golang.org/x/net/html/charset"
 )
 
@@ -35,40 +37,31 @@ func findTitle(htmlBody []byte) (title string, err error) {
 	if err != nil {
 		return "", err
 	}
-	node, err := html.Parse(bodyReader)
-	if err != nil {
-		return "", err
-	}
-	if t, ok := getFirstElement(node, "title"); ok {
-		return t, nil
-	}
-	return "", errNoTitleTag
-}
-
-// getFirstElement returns flattened content of first found element of given
-// type
-func getFirstElement(n *html.Node, element string) (t string, found bool) {
-	if n.Type == html.ElementNode && n.Data == element {
-		return flatten(n), true
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		t, found = getFirstElement(c, element)
-		if found {
-			return
+	z := html.NewTokenizer(bodyReader)
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			if z.Err() == io.EOF {
+				goto notFound
+			}
+			return "", z.Err()
+		case html.StartTagToken:
+			name, _ := z.TagName()
+			switch atom.Lookup(name) {
+			case atom.Body:
+				goto notFound // title should preceed body tag
+			case atom.Title:
+				if tt := z.Next(); tt == html.TextToken {
+					return string(z.Text()), nil
+				} else {
+					goto notFound
+				}
+			}
 		}
 	}
-	return
-}
-
-// flatten returns flattened text content of html node
-func flatten(n *html.Node) (res string) {
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		res += flatten(c)
-	}
-	if n.Type == html.TextNode {
-		return n.Data
-	}
-	return
+notFound:
+	return "", errNoTitleTag
 }
 
 var (
