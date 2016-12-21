95d01bf4ec315a8d73aacdd6c5c53c2ad460c703
Artyom Pervukhin <artyom@evasive.ru>
2016-12-21 19:47:00 +0300

    Customize http.Transport

--- cmd/unfurlist/unfurlist.go
+++ cmd/unfurlist/unfurlist.go
@@ -52,8 +52,19 @@ func main() {
 	config := unfurlist.Config{
 		HTTPClient: &http.Client{
 			CheckRedirect: failOnLoginPages,
-			Transport:     http.DefaultTransport,
 			Timeout:       timeout,
+			Transport: &http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   10 * time.Second,
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			},
 		},
 		Headers:        []string{"Accept-Language", "en;q=1, *;q=0.5"},
 		Log:            log.New(os.Stderr, "", log.LstdFlags),
